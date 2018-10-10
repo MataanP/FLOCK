68f63c24688dfe85ed3d8635d38de47736b87328
Alaska <lillywiz@gmail.com>
2018-10-09 20:27:54 -0600

    boid logic for choosing direction performance tested

--- Boid.py
+++ Boid.py
@@ -1,27 +1,64 @@
-import directions
+import time
 import numpy as np
+#these functions move birds with [x, y] numpy arrays
+
+steps = int(1)
+#horizontal/vertical movements
+#x coordinate postive shift, no y shift
+def move_right(arr):
+    arr[0] += steps
+
+#x coordinate negative shift, no y shift
+def move_left(arr):
+    arr[0] -= steps
+
+#no x shift, y coordinate positive shift
+def move_up(arr):
+    arr[1] += steps
+
+#no x shift, y coordinate negative shift
+def move_down(arr):
+    arr[1] -= steps
+
+
+#diagnoal movements
+def move_top_right(arr):
+    arr[0] += steps
+    arr[1] += steps
+
+def move_top_left(arr):
+    arr[0] -= steps
+    arr[1] += steps
+
+def move_bot_left(arr):
+    arr[0] -= steps
+    arr[1] -= steps
+
+def move_bot_right(arr):
+    arr[0] += steps
+    arr[1] -= steps
 
 #Split the vertical/horizontal movement to its own logic check
 def move_xy(boid, a_boid):
     if boid[0] == a_boid[0] and boid[1] < a_boid[1]:
-        move_down(boid)
+        boid = move_down(boid)
     elif boid[0] == a_boid[0] and boid[1] > a_boid[1]:
-        move_up(boid)
+        boid = move_up(boid)
     elif boid[1] == a_boid[1] and boid[0] < a_boid[0]:
-        move_right(boid)
+        boid = move_right(boid)
     elif boid[1] == a_boid[1] and boid[0] > a_boid[0]:
-        move_left(boid)
+        boid = move_left(boid)
 
 #Split the diag movement to its own logic check
 def move_diag(boid, a_boid):
     if boid[1] > a_boid[1] and boid[0] > a_boid[0]:
-        move_bot_left(boid)
+        boid = move_bot_left(boid)
     elif boid[1] > a_boid[1] and boid[0] < a_boid[0]:
-        move_bot_right(boid)
+        boid = move_bot_right(boid)
     elif boid[1] < a_boid[1] and boid[0] > a_boid[0]:
-        move_top_left(boid)
+        boid = move_top_left(boid)
     elif boid[1] < a_boid[1] and boid[0] < a_boid[0]:
-        move_top_right(boid)
+        boid = move_top_right(boid)
 
 #Decide the most efficient move based on coordinates
 def decide_move(boid, a_boid):
@@ -30,11 +67,16 @@ def decide_move(boid, a_boid):
     else:
         move_diag(boid, a_boid)
 
-def main():
-    boids1 = np.array([4, 1])
-    boids2 = np.array([1, 2])
-    vfunc = np.vectorize(decide_move(boids1, boids2))
+#Loop continually moves boid closer to alpha using vectorized funciton
+# def getting_closer(boid, a_boid):
+#     while np.array_equal(boid, a_boid) == False:
+#         vfunc = np.vectorize(decide_move(boid, a_boid))
+#         print(str(boid) + str(a_boid))
 
+def main():
+    boid = np.array([7, 2])
+    a_boid = np.array([2, 0])
+    vfunc = np.vectorize(decide_move(boid, a_boid))
 
 if __name__ == "__main__":
     main()
