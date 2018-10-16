51a2331242c52e41b8e727a627418dff38055768
Alaska <lillywiz@gmail.com>
2018-10-16 00:23:21 -0600

    move scratches away

--- Scratch Python/Boid.py
+++ Scratch Python/Boid.py
@@ -1,46 +1,45 @@
 import numpy as np
-import directions
-from scipy.ndimage import measurements
 
 #Split the vertical/horizontal movement to its own logic check
-def a_move_xy(boid, a_boid):
+def move_xy(boid, a_boid):
     if boid[0] == a_boid[0] and boid[1] < a_boid[1]:
-        a_boid = move_down(a_boid)
+        boid = move_down(boid)
     elif boid[0] == a_boid[0] and boid[1] > a_boid[1]:
-        a_boid = move_up(a_boid)
+        boid = move_up(boid)
     elif boid[1] == a_boid[1] and boid[0] < a_boid[0]:
-        a_boid = move_right(a_boid)
+        boid = move_right(boid)
     elif boid[1] == a_boid[1] and boid[0] > a_boid[0]:
-        a_boid = move_left(a_boid)
+        boid = move_left(boid)
 
 #Split the diag movement to its own logic check
-def a_move_diag(boid, a_boid):
+def move_diag(boid, a_boid):
     if boid[1] > a_boid[1] and boid[0] > a_boid[0]:
-        a_boid = move_bot_left(a_boid)
+        boid = move_bot_left(boid)
     elif boid[1] > a_boid[1] and boid[0] < a_boid[0]:
-        a_boid = move_bot_right(a_boid)
+        boid = move_bot_right(boid)
     elif boid[1] < a_boid[1] and boid[0] > a_boid[0]:
-        a_boid = move_top_left(a_boid)
+        boid = move_top_left(boid)
     elif boid[1] < a_boid[1] and boid[0] < a_boid[0]:
-        a_boid = move_top_right(a_boid)
+        boid = move_top_right(boid)
 
 #Decide the most efficient move based on coordinates
-def a_decide_move(boid, a_boid):
+def decide_move(boid, a_boid):
     if boid[0] == a_boid[0] or boid[1] == a_boid[1]:
-        a_move_xy(boid, a_boid)
+        move_xy(boid, a_boid)
     else:
-        a_move_diag(boid, a_boid)
+        move_diag(boid, a_boid)
+
+#Loop continually moves boid closer to alpha using vectorized funciton
+# def getting_closer(boid, a_boid):
+#     while np.array_equal(boid, a_boid) == False:
+#         vfunc = np.vectorize(decide_move(boid, a_boid))
+#         print(str(boid) + str(a_boid))
 
 def main():
+    boid = np.array([7, 2])
     a_boid = np.array([2, 0])
-    boid_arr = np.random.randint(-50, 50, size=(4, 2))
-    centroid = np.rint(measurements.center_of_mass(boid_arr))
-    print(centroid)
-    vfunc = np.vectorize(a_decide_move(centroid, a_boid))
-    print(a_boid)
+    vfunc = np.vectorize(decide_move(boid, a_boid))
 
 if __name__ == "__main__":
     main()
-
-
 
